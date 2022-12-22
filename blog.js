422478c59dbd057ec888f749103a67ea832ba9b6
Will Cooke <willcooke2004@gmail.com>
2022-12-22 12:37:43 +0000

    Bugfixing

--- blog.js
+++ blog.js
@@ -1,6 +1,6 @@
 $.getJSON('blog.json', function(data){
     $.each(data.article, function(i, f) {
-        var article = "<div class='article'><div class='article_title'><div class='box_left'>" + data.title + "</div><div class='box_right'>" + data.date + "</div></div><p>" + data.content + "</p></div><br>";
-        $(article).appendTo('#blog');
+        //var article = "<div class='article'><div class='article_title'><div class='box_left'>" + data.title + "</div><div class='box_right'>" + data.date + "</div></div><p>" + data.content + "</p></div><br>";
+        $("<div class='article'><div class='article_title'><div class='box_left'>" + data.title + "</div><div class='box_right'>" + data.date + "</div></div><p>" + data.content + "</p></div><br>").appendTo('#blog');
     });
 });
