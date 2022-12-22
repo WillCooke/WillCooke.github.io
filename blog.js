2c7ceaff048ed68df22aa22670e0fe6584d5caf2
Will Cooke <willcooke2004@gmail.com>
2022-12-22 18:51:11 +0000

    Updated styling

--- blog.js
+++ blog.js
@@ -1,4 +1,4 @@
-$.getJSON('blog.json', function(data){
+$.getJSON('https://willcooke.github.io/blog.json', function(data){
     $.each(data.article, function(i, f) {
         var article = "<div class='article'><div class='article_title'><div class='box_left'>" + f.title + "</div><div class='box_right'>" + f.date + "</div></div><p>" + f.content + "</p></div><br>";
         $(article).appendTo('#blog');
